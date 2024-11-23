5e4fa8bf3650d112b651bf35800e011a4924de16
Chandan Jal <chandan-jal-evolution>
2024-11-23 12:08:37 +0530

    feat: prisma client updated

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,3 +1,28 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// Extend the globalThis interface
+declare global {
+  // Extending the global object for the Node.js environment
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      __prisma?: PrismaClient;
+    }
+  }
+
+  // eslint-disable-next-line no-var
+  var __prisma: PrismaClient | undefined; // For usage with globalThis
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.__prisma) {
+    global.__prisma = new PrismaClient();
+  }
+  prisma = global.__prisma;
+}
+
+export { prisma };
